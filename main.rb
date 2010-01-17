0bed2af459187f459e94bee7760e574e7bdbe92e
Keith Driver <kdriver@tduk.com0>
2010-01-17 22:30:47 +0000

    connecting

--- main.rb
+++ main.rb
@@ -1,11 +1,32 @@
 require 'socket'      # Sockets are in standard library
+require 'MM'      
 
-hostname = '192.168.0.2'
-port = 80
+def smr_bind
+	msg = MM.new
+	smr = SM.new
+  
+	msg.add(smr.create_smr_bind)
+	smr_bind_request = msg.get_buffer
+	return smr_bind_request
+end
+
+hostname = '172.16.6.166'
+port = 9001
 
 s = TCPSocket.open(hostname, port)
 
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
-end
-s.close               # Close the socket when done
+
+buffer = smr_bind()
+
+buffer.each { |i| print i.to_s , "," }
+print "\n"
+
+s.write(buffer)
+
+print "written\n"
+
+reply = s.read()
+
+print "read\n"
+
+reply.each { |i| print i.to_s , "," }
